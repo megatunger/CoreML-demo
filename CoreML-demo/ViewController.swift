0a0be28ddb70577ffbc100dcbaa6fbba81245cfe
megatunger <megatunger@gmail.com>
2019-02-20 10:34:14 +0700

    Initial Commit

---
+++ CoreML-demo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CoreML-demo
+//
+//  Created by Hoang Son Tung on 2/20/19.
+//  Copyright © 2019 Hoang Son Tung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // C
+    }
+
+
+}
+
